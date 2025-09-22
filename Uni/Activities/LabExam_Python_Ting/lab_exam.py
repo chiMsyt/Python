0543f014570cc587aeda387c3b635f4a0c32ab83
chiMsyt <timothyting2@gmail.com>
2025-09-22 17:17:45 +0800

    Added preliminary exam, as well as my 6th project (on going).

---
+++ Uni/Activities/LabExam_Python_Ting/lab_exam.py
@@ -0,0 +1,32 @@
+# Preliminary Exam
+
+# Step 1 - Welcome message
+
+welcomeMessage = "Hello! Welcome to Python."
+print(welcomeMessage)
+
+# Step 2 - Variables
+
+firstName = "Timothy"
+age = 22
+GPA = 1.5
+
+introduction = f"My name is {firstName}, and I am currently {age} years old. My GPA is {GPA}."
+print(introduction)
+
+# Step 3 - Data Types
+
+print(type(firstName), type(age), type(GPA))
+
+# Step 4 - Type Conversion
+
+howOld = input("How old are you?: ")
+fiveYears = f"After 5 years, you are: {int(howOld) + 5} years old."
+print(fiveYears)
+
+# First single-line comment
+# Second single-line comment
+
+""" 
+this is my one and only multi-line comment :)
+"""
