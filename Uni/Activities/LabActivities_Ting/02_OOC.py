8201c487d1fa1cceda963adc6faa7735d9a37c65
chiMsyt <timothyting2@gmail.com>
2025-10-13 23:03:01 +0800

    Added ** to numbers 1 and 2 at the end, was missing from the docx

--- Uni/Activities/LabActivities_Ting/02_OOC.py
+++ Uni/Activities/LabActivities_Ting/02_OOC.py
@@ -2,14 +2,14 @@
 
 Task 2: The Order of Operations Challenge
 Without running the code first, predict the output of the following expressions. Then, write a program to check your answers.
-1.  result1 = 5 + 3 * 2  2
-2.  result2 = (5 + 3) * 2  2
+1.  result1 = 5 + 3 * 2 ** 2
+2.  result2 = (5 + 3) * 2 ** 2
 3.  result3 = 10 % 3 + 5 * 2
 
 '''
 
-result1 = 5 + 3 * 2  # 11
-result2 = (5 + 3) * 2  # 16
+result1 = 5 + 3 * 2 ** 2 # 17
+result2 = (5 + 3) * 2 ** 2 # 32
 result3 = 10 % 3 + 5 * 2 # 11
 
 print(f"result1: {result1}, result2: {result2}, result3: {result3}")
